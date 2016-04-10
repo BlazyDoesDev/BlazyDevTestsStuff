f4a71e12874ed2e15fb2be30e2fdc48611154ad2
BlazyDoesDev <DragonToothyBeam@outlook.com>
2016-04-10 18:49:51 +0000

    Added 01

---
+++ PythonTests/01 - Print Test.py
@@ -0,0 +1,8 @@
+# Prints hello world to console
+print("Hello World!")
+
+# CHALLANGE - Whats wrong with this // Make new folder in this folder called your usename
+# And copy this file  to the new folder you just made, and edit it with the solution 
+
+
+print("Hello World!"
